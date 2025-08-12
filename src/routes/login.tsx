88172f6c25f7fc5b5d964433b970011927df48bb
emersonpisoni <emersonpisoni14@gmail.com>
2025-08-11 23:18:38 -0300

    Change login title

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -15,7 +15,7 @@ function LoginPage() {
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <GalleryVerticalEnd className="size-4" />
             </div>
-            Emershow dev.
+            DevelopEmerson.
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
