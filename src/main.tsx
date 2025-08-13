2c730e33388afd3074015e2419b6ed7841bf20fa
emersonpisoni <emersonpisoni14@gmail.com>
2025-08-12 23:16:33 -0300

    Remove comment

--- src/main.tsx
+++ src/main.tsx
@@ -23,7 +23,6 @@ declare module '@tanstack/react-router' {
   }
 }
 
-// Render the app
 const rootElement = document.getElementById('root')
 if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
